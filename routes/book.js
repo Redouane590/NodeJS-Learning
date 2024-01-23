cf477aa141fb8c48de0d16eced108c7d3b524f26
REDOUANE ELMAHI <redouaneelmahi@gmail.com>
2024-01-23 16:23:32 +0100

    Refacto all structure, Add image upload functionality and update dependencies

--- routes/book.js
+++ routes/book.js
@@ -1,25 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bookCtrl = require('../controllers/book');
+const bookCtrl = require("../controllers/book");
+const auth = require("../middleware/auth");
+const multer = require('../middleware/multer-config');
 
 
+router.get("/bestrating", bookCtrl.getBestrating);
 
+router.post("/:id/rating", auth, bookCtrl.createRating);
 
-router.get('/', bookCtrl.getBooks);
+router.get("/", bookCtrl.getBooks);
 
-router.get('/:id', (bookCtrl.getBook));
+router.get("/:id", bookCtrl.getBook);
 
-router.get('/bestrating', bookCtrl.getBestrating);
+router.post("/", auth, multer, bookCtrl.createBook);
 
-router.post('/', bookCtrl.createBook);
+router.put("/:id", auth, multer, bookCtrl.updateBook);
 
-router.put('/:id', bookCtrl.updateBook);
-
-router.delete('/:id', bookCtrl.deleteBook);
-
-router.post('/:id/rating', bookCtrl.createRating);
-
-
+router.delete("/:id", auth, bookCtrl.deleteBook);
 
 
 module.exports = router;
